2439a06c14e5cc492c1d6ad09155dd3fcb6fd032
Keenan Graham <keenangraham@stanford.edu>
2017-11-03 15:50:45 -0700

    Test ENTEx samples skipped in NIH consent audit

--- src/encoded/tests/test_audit_library.py
+++ src/encoded/tests/test_audit_library.py
@@ -22,3 +22,18 @@ def test_audit_library_with_nih_consent(testapp, library, encode4_award):
         errors_list.extend(errors[error_type])
     assert all(error['category'] != 'missing nih_consent'
                for error in errors_list)
+
+
+def test_audit_library_missing_nih_consent_skip_entex(testapp,
+                                                      library,
+                                                      encode4_award,
+                                                      biosample):
+    testapp.patch_json(library['@id'], {'award': encode4_award['@id']})
+    testapp.patch_json(biosample['@id'], {'internal_tags': ['ENTEx']})
+    res = testapp.get(library['@id'] + '@@index-data')
+    errors = res.json['audit']
+    errors_list = []
+    for error_type in errors:
+        errors_list.extend(errors[error_type])
+    assert all(error['category'] != 'missing nih_consent'
+               for error in errors_list)
